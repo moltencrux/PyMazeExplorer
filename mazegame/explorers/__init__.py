ac9ede1e935a7822671e6075f26689281a534707
moltencrux <moltencrux@gmail.com>
2026-09-07 00:08:00 +0800

    feat: add DFS explorer

--- mazegame/explorers/__init__.py
+++ mazegame/explorers/__init__.py
@@ -1,7 +1,9 @@
 from .random_walk import RandomWalkExplorer
 from .wall_follower import WallFollowerExplorer
+from .dfs import DFSExplorer
 
 __all__ = [
     "RandomWalkExplorer",
     "WallFollowerExplorer",
+    "DFSExplorer",
 ]
